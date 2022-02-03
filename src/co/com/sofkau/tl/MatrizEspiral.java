b60c37e3032583e39916f689f99b66337b31f475
virginia <vgiardino@outlook.es>
2022-02-03 15:28:07 -0300

    Fila der-izq

---
+++ src/co/com/sofkau/tl/MatrizEspiral.java
@@ -0,0 +1,23 @@
+package co.com.sofkau.tl;
+
+public class MatrizEspiral {
+    // TODO: 2/02/2022 método para recorrer fila izq-der
+    // TODO: 2/02/2022 método para recorrer fila der-izq
+    // TODO: 2/02/2022 método para recorrer columna desc
+    // TODO: 2/02/2022 método para recorrer columna asc
+    // TODO: 2/02/2022 método para solicitar tamaño de matriz a llenar
+    // TODO: 2/02/2022 método para imprimir la matriz
+    // TODO: 2/02/2022 método para integrar todas las funcionalidades anteriores por medio de un menú que se comunique con el usuario
+
+    // Felipe Munevar - Virginia Giardino
+
+    public static int[][] llenarFilaDerIzq (int[][] matriz, int nroFila, int nroInicio) {
+        for (int i = matriz[0].length-1; i >= 0; i--) {
+            if (matriz[nroFila][i] == 0) {
+                matriz[nroFila][i] = nroInicio;
+                nroInicio++;
+            }
+        }return matriz;
+    }
+}
+
